18f236cd68c5dd6b68686812868bf54d0052bbd2
Omar Garcia <og358513@gmail.com>
2025-10-31 02:57:11 -0600

    parte 5

---
+++ src/Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora {
+public static int sumar(int a, int b) {
+return a + b;
+}
+public static int restar(int a, int b) {
+return a - b;
+}
+public static int multiplicar(int a, int b) {
+return a * b;
+}
+public static int dividir(int a, int b) {
+if (b == 0) throw new ArithmeticException("No se puede dividir entre cero");
+return a / b;
+}
+}
